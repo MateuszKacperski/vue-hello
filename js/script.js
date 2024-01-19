5ffbd05fe3a1cddc2dad16a25047d554cd504249
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-01-19 13:30:56 +0100

    added step 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log('Vue ok', Vue);
+
+const {createApp} = Vue;
+const app = createApp({
+    name: 'Vue Hello',
+    data(){
+        return {
+            message: 'Hello Vue',
+           
+        }
+    }
+
+})
+app.mount('#root')
